e4bf7f1b6f024e4746c5aa69b0d9843bd0ae0597
Michael V. DePalatis <mike@depalatis.net>
2018-05-16 10:56:44 -0400

    Import wrappers from ramutils

--- cml_pipelines/__init__.py
+++ cml_pipelines/__init__.py
@@ -1,4 +1,10 @@
 from collections import namedtuple
+import logging
+
+from .wrapper import task, make_task
 
 __version__ = '0.1.0'
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
+
+logger = logging.getLogger("cml.pipelines")
+logger.addHandler(logging.NullHandler())
