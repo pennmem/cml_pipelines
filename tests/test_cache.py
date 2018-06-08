14bfae3e78ca33ce3fef23de84784671f3dcaeb0
Michael V. DePalatis <mike@depalatis.net>
2018-06-08 17:13:56 -0400

    Convert LocalPath to str

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -3,21 +3,22 @@ from cml_pipelines.cache import memory
 
 
 def test_cachedir(tmpdir):
-    memory.cachedir = tmpdir
+    memory.cachedir = str(tmpdir)
 
     @memory.cache
     def doit(p):
         return p
 
     doit(1)
-    assert os.path.exists(tmpdir.join("test_cache").join("doit"))
+    assert os.path.exists(str(tmpdir.join("test_cache").join("doit")))
 
     # change to a new location
-    memory.cachedir = tmpdir.join("other")
+    memory.cachedir = str(tmpdir.join("other"))
 
     @memory.cache
     def doit2(p):
         return p
 
     doit2(1)
-    assert os.path.exists(tmpdir.join("other").join("test_cache").join("doit2"))
+    newpath = str(tmpdir.join("other").join("test_cache").join("doit2"))
+    assert os.path.exists(newpath)
