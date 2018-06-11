1a8a6487aea85379b6a0651b6d62adf8164e7a67
Michael V. DePalatis <mike@depalatis.net>
2018-06-10 20:39:42 -0400

    Add basic pipeline class

---
+++ cml_pipelines/pipeline.py
@@ -0,0 +1,39 @@
+from concurrent.futures import Future, ThreadPoolExecutor
+
+from dask.delayed import Delayed
+
+from .cache import memory
+
+
+class Pipeline(object):
+    """Base class for building pipelines.
+
+    Parameters
+    ----------
+    clear_cache_on_completion
+        When True (the default), clear the cache upon successful completion.
+
+    """
+    executor = ThreadPoolExecutor(max_workers=1)
+
+    def __init__(self, clear_cache_on_completion: bool = True):
+        self.clear_cache_on_completion = clear_cache_on_completion
+
+    def build(self) -> Delayed:
+        """Override this method to define a pipeline. This method must return a
+        :class:`Delayed` instance. This is most easily accomplished by returning
+        the result of a function wrapped with the ``task``.
+
+        """
+        raise NotImplementedError
+
+    def run(self) -> Future:
+        """Run the pipeline. Returns a :class:`Future` which will contain the
+        result.
+
+        """
+        pipeline = self.build()
+        future = self.executor.submit(pipeline.compute)
+        if self.clear_cache_on_completion:
+            future.add_done_callback(lambda f: memory.clear(warn=False))
+        return future
---
+++ tests/test_pipeline.py
@@ -0,0 +1,30 @@
+from concurrent.futures import Future
+import os
+from unittest.mock import patch
+
+import pytest
+from sklearn.externals.joblib import Memory
+
+from cml_pipelines import task
+from cml_pipelines.pipeline import Pipeline
+
+
+class MyPipeline(Pipeline):
+    @task()
+    def add(self, a, b):
+        return a + b
+
+    def build(self):
+        return self.add(1, 1)
+
+
+class TestPipeline:
+    @pytest.mark.parametrize("clear_cache", [True, False])
+    def test_run(self, clear_cache):
+        with patch.object(Memory, "clear") as clear_func:
+            pipeline = MyPipeline(clear_cache)
+            future = pipeline.run()
+            assert isinstance(future, Future)
+            assert future.result(timeout=1) == 2
+            if clear_cache:
+                clear_func.assert_called_once()
